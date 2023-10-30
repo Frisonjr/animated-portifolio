aa7186df9d46cdd4868a9dc3e533cb833c450d2a
FrisonJr <jair.frison@gmail.com>
2023-10-30 15:54:24 -0300

    finished portifolio

---
+++ components/Socials.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import {
+  RiYoutubeLine,
+  RiFacebookLine,
+  RiInstagramLine,
+  RiGithubLine,
+  RiLinkedinLine,
+  RiPinterestLine,
+} from "react-icons/ri";
+
+const Socials = () => {
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiPinterestLine />
+      </Link>
+    </div>
+  );
+};
+
+export default Socials;
